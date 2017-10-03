318dc80e3bd862f86c0ec15fbd814de7d5231de0
Caleb Mitchell <soccer_imp@hotmail.com>
2017-10-03 14:18:28 +1300

    Added comboBox for changing between PDFs

--- PDF_Viewer/AppDelegate.swift
+++ PDF_Viewer/AppDelegate.swift
@@ -82,38 +82,43 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     
     
-    //
-    //  Action functions for UI objects
-    //
+
+    /*********************************************************
+     
+                Action functions for UI objects
+     
+    *********************************************************/
     
-    // Open documents and start a timer to update page number
+    // Open documents and start a timer that calls updatePageNumber
     @IBAction func openPDF(sender: NSMenuItem) {
         if let urls = NSOpenPanel().selectURL {
-            // update page number every 0.1 seconds
-            _ = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.updatePageNumber), userInfo: nil, repeats: true)
             thePDF.document = PDFClass(url: urls.first!)
             documentOpen = true
+            currentDoc += 1
             for doc in urls{
                 chooseDocBox.addItem(withObjectValue: doc.lastPathComponent)
                 documents.append(PDFClass(url: doc)!)
             }
             chooseDocBox.stringValue = (urls.first?.lastPathComponent)!
+            
+            // update page number every 0.1 seconds
+            _ = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.updatePageNumber), userInfo: nil, repeats: true)
         }
     }
     
     // Zoom in action
     @IBAction func zoomIn(_ sender: NSButton) {
-        self.thePDF.zoomIn(sender)
+        thePDF.zoomIn(sender)
     }
     
     // Zoom out action
     @IBAction func zoomOut(_ sender: NSButton) {
-        self.thePDF.zoomOut(sender)
+        thePDF.zoomOut(sender)
     }
     
     // Fits the document view to screen
     @IBAction func resetZoom(_ sender: NSButton) {
-        self.thePDF.autoScales = true
+        thePDF.autoScales = true
     }
     
     // Goes to the previous page
@@ -139,21 +144,37 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             }
         }
     }
+    
+    // Changes displayed document to users selection
     @IBAction func PDFSelect(_ sender: NSComboBox) {
         if(chooseDocBox.indexOfSelectedItem >= 0) {
             let index: Int = chooseDocBox.indexOfSelectedItem
             thePDF.document = documents[index]
+            currentDoc = index+1
+            updateDocument(sender: thePDF.document!)
         }
     }
     
-    //
-    //  Helper functions
-    //
     
+    
+    /*********************************************************
+
+                         Helper functions
+
+    **********************************************************/
+    
+    // Updates the page number text field
+    // - Called every 0.1s by timer
     func updatePageNumber() {
         let page = thePDF.currentPage
         let pageNum = (thePDF.document?.index(for: page!))! + 1
         pageNumberField.stringValue = "Page \(pageNum) of \(thePDF.document?.pageCount ?? 0)"
+    }
+    
+    // Updates variables after changing document
+    func updateDocument(sender: PDFDocument) {
+        appWindow.title = (sender.documentURL?.lastPathComponent)!
+        
     }
     
 }
